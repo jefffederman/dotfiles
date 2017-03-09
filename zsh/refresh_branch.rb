da48b29fb0b53f4ee11c24d2cc9830899fcb20e7
Jeff Federman <jefffederman@gmail.com>
2017-03-08 22:44:34 -0500

    More robust refresh_branch

--- zsh/refresh_branch.rb
+++ zsh/refresh_branch.rb
@@ -1,9 +1,8 @@
 current_branch = `git rev-parse --abbrev-ref HEAD`.strip
-other_branches = `git branch`.gsub("* #{current_branch}\n", '')
-                             .split("\n").map(&:strip)
-random_branch = other_branches.sample
-`git checkout #{random_branch}`
+temp_branch = "temp-#{current_branch}"
+`git checkout -b #{temp_branch}`
 `git br -D #{current_branch}`
 `git fetch upstream #{current_branch}:#{current_branch}`
 `git checkout #{current_branch}`
+`git branch -D #{temp_branch}`
 `git push -f origin HEAD`
