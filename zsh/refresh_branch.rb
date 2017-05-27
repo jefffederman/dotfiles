dadc4a2998bb8ffbd9db2aff5080b7a88f0257c7
Jeff Federman <jefffederman@gmail.com>
2017-05-27 19:22:12 -0400

    Account for no upstream in `refresh_branch`

--- zsh/refresh_branch.rb
+++ zsh/refresh_branch.rb
@@ -2,7 +2,12 @@ current_branch = `git rev-parse --abbrev-ref HEAD`.strip
 temp_branch = "temp-#{current_branch}"
 `git checkout -b #{temp_branch}`
 `git br -D #{current_branch}`
-`git fetch upstream #{current_branch}:#{current_branch}`
+remote = if !`git ls-remote upstream | ag #{current_branch}`.empty?
+           "upstream"
+         else
+           "origin"
+         end
+`git fetch #{remote} #{current_branch}:#{current_branch}`
 `git checkout #{current_branch}`
 `git branch -D #{temp_branch}`
-`git push -f origin HEAD`
+`git push -f origin HEAD` if remote == "upstream"
