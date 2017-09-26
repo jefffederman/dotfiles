e45614ecf23230108178b230c428c972299dac33
Jeff Federman <jefffederman@gmail.com>
2017-09-25 22:08:37 -0400

    Rails template WIP

--- rails_template.rb
+++ rails_template.rb
@@ -8,3 +8,20 @@ end
 gem_group :development do
   gem 'awesome_print'
 end
+
+application do <<~EOL
+  config.generators.assets false
+  config.generators.helper false
+  config.generators.controller_specs false
+  config.generators.view_specs false
+  config.generators.orm :active_record, primary_key_type: :uuid
+  EOL
+end
+
+# bin/rails g rspec:install
+# bin/rails g migration enable_pgcrypto_extension ->
+# class EnablePgcryptoExtension < ActiveRecord::Migration[5.1]
+  # def change
+    # enable_extension 'pgcrypto'
+  # end
+# end
